acb0bbb2bf44159459a58331ca3cc975508f6aa0
unknown <kate.khlystunova@gmail.com>
2020-08-16 13:39:03 +0300

    init

---
+++ src/main/java/org/khl/SimpleChat/SimpleChatApplication.java
@@ -0,0 +1,13 @@
+package org.khl.SimpleChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChatApplication.class, args);
+	}
+
+}
